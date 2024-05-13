7b73e82b3b54319d153f0355f383cba421b47160
SeekerDev <albertbaisa@gmail.com>
2024-05-13 13:40:54 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Side from "./sidebar/Side";
 import Main from "./main/Main";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <div className="h-screen w-screen bg-[#a4bbb9] md:p-10 p-2">
       <div className="flex h-full w-full bg-slate-100 rounded-xl shadow-lg shadow-slate-700">
+        <Analytics />
         <Side />
         <Main />
       </div>
